49a5073f481f5b58cf8fefcd22dea8d4fcdada6f
emiliaszymanska <esz.emilia@gmail.com>
2021-03-14 22:54:55 +0100

    Home page - closing section

---
+++ src/home/common/Closing.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+function Closing() {
+    return (
+        <>
+            <section className="content-closing">
+                <div className="layout-container">
+                    <div className="layout-component-center">
+                        <h2>Foster your creativity</h2>
+                        <p className="paragraph-description">Embark on a journey</p>
+
+                        <div className="element-cta layout-flex">
+                            <a href="#" className="element-button button-standard button-first">
+                                Sign up now</a>
+                            <p className="small-text"><small>Studer is free.</small></p>
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </>
+    );
+}
+
+export default Closing;
