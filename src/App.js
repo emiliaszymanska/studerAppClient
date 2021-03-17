111eee364e2acb8883e9750cb0c8c8c43c6fce95
emiliaszymanska <esz.emilia@gmail.com>
2021-03-17 23:55:55 +0100

    Add App.js and Config.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 import React from "react";
+import Header from "./shared/header/Header";
+import Home from "./home/Home";
+import Map from "./map/Map";
+import Footer from "./shared/Footer";
+
+import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
 
 function App() {
     return (
-        <>
-        </>
+        <Router>
+            <Header/>
+            <Switch>
+                <Route path="/" exact component={Home}/>
+                <Route path="/map" component={Map}/>
+            </Switch>
+            <Footer/>
+        </Router>
     );
 }
 
