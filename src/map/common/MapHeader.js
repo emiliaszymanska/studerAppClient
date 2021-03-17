005a38581399298bfc4ae3a2249ae63656ff59f0
emiliaszymanska <esz.emilia@gmail.com>
2021-03-18 00:09:05 +0100

    Add MapHeader.js with simple header

---
+++ src/map/common/MapHeader.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function MapHeader() {
+    return (
+        <>
+            <div className="map-header layout-flex">
+                <p className="paragraph-description">Map - important spots for English-speaking students</p>
+                <h2>Your journey starts here</h2>
+            </div>
+        </>
+    );
+}
+
+export default MapHeader;
