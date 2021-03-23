3d8a042ab484464439690ae29a636aab0c282a13
emiliaszymanska <esz.emilia@gmail.com>
2021-03-23 16:39:20 +0100

    Categories.js - add a handleCheckboxChange function

--- src/map/common/Categories.js
+++ src/map/common/Categories.js
@@ -3,14 +3,23 @@ import {GetMapCategories} from "../services/MapApiService"
 import {CategoryModel} from "../model/CategoryModel";
 
 function Categories() {
+
     const [categories, setCategories] = useState([]);
 
-    useEffect( () => {
+    useEffect(() => {
+        // console.log("useEffect")
         GetMapCategories().then(res => {
             const categoriesModel = res?.data?.map(item => new CategoryModel(item))
             setCategories(categoriesModel)
         });
-    });
+    }, []);
+
+    function handleCheckboxChange(e) {
+        const categoriesCopy = [...categories];
+        categoriesCopy.filter(category => category.type === e.target.id)
+            .map(category => category.isChecked = e.target.checked)
+        setCategories(categoriesCopy);
+    }
 
     return (
         <>
@@ -18,8 +27,10 @@ function Categories() {
                 <div className="checkbox-wrapper layout-grid">
                     {categories.map(category => (
                         <div key={category?.type}>
-                            <input type="checkbox" id={category?.type} value={category?.type}/>
+                            <input type="checkbox" id={category?.type} value={category?.type}
+                                   onClick={handleCheckboxChange}/>
                             <label htmlFor={category?.type}>{category?.name} {category?.isChecked}</label>
+                            {/*{JSON.stringify(category)}*/}
                         </div>
                     ))}
                 </div>
