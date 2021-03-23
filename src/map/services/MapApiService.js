4ee108ebe5668611352af03d3c18df1478ffc101
emiliaszymanska <esz.emilia@gmail.com>
2021-03-23 16:40:01 +0100

    MapApiService.js - change the URL (GetMapLocations)

--- src/map/services/MapApiService.js
+++ src/map/services/MapApiService.js
@@ -1,9 +1,9 @@
 import axios from "axios";
 import {API_URL} from "../../environment/Config";
 
-export const GetMapLocations = () => {
+export const GetMapLocations = (placeType, centerLatitude, centerLongitude, radius) => {
     return axios
-        .get(`${API_URL}api/place/`);
+        .get(`${API_URL}api/place/type/radius/${placeType}?centerLatitude=${centerLatitude}&centerLongitude=${centerLongitude}&radius=${radius}`);
 }
 
 export const GetMapCategories = () => {
