a727610e3bec929adb99f42bba18ce2724051abc
emiliaszymanska <esz.emilia@gmail.com>
2021-03-24 15:47:21 +0100

    Add a category filtering functionality

--- src/map/Map.js
+++ src/map/Map.js
@@ -1,16 +1,19 @@
-import React from 'react';
+import React, {useState} from 'react';
 import MapHeader from "./common/MapHeader";
 import Categories from "./common/Categories";
 import MapContent from "./common/MapContent";
 
 function Map() {
+
+    const [categoriesDTO, setCategoriesDTO] = useState([]);
+
     return (
         <>
             <section className="content-map">
                 <div className="layout-container">
                     <MapHeader />
-                    <Categories />
-                    <MapContent />
+                    <Categories onCategoriesSelect={(e) => setCategoriesDTO(e)} />
+                    <MapContent categories={categoriesDTO}/>
                 </div>
             </section>
         </>
--- src/map/services/MapApiService.js
+++ src/map/services/MapApiService.js
@@ -1,9 +1,9 @@
 import axios from "axios";
 import {API_URL} from "../../environment/Config";
 
-export const GetMapLocations = (placeType, centerLatitude, centerLongitude, radius) => {
+export const GetMapLocations = (placeType = [], centerLatitude, centerLongitude, radius) => {
     return axios
-        .get(`${API_URL}api/place/type/radius/${placeType}?centerLatitude=${centerLatitude}&centerLongitude=${centerLongitude}&radius=${radius}`);
+        .get(`${API_URL}api/place/type/radius/${placeType.join(',')}?centerLatitude=${centerLatitude}&centerLongitude=${centerLongitude}&radius=${radius}`);
 }
 
 export const GetMapCategories = () => {
