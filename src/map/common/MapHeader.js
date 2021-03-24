3dd3aa5e9fc23bcd83056e3f02a8ee58f2688f0f
emiliaszymanska <esz.emilia@gmail.com>
2021-03-24 18:37:17 +0100

    MapHeader.js - minor change

--- src/map/common/MapHeader.js
+++ src/map/common/MapHeader.js
@@ -4,7 +4,7 @@ function MapHeader() {
     return (
         <>
             <div className="map-header layout-flex">
-                <p className="paragraph-description">Map - important spots for English-speaking students</p>
+                <p className="paragraph-description">Map – important spots for English-speaking students</p>
                 <h2>Your journey starts here</h2>
             </div>
         </>
