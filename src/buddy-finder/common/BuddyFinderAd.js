66dc8d14543af8a98903f5592a7e8a9b62e4ad9d
emiliaszymanska <esz.emilia@gmail.com>
2021-03-31 19:57:53 +0200

    BuddyFinderAd.js - Details button acts like a link now

--- src/buddy-finder/common/BuddyFinderAd.js
+++ src/buddy-finder/common/BuddyFinderAd.js
@@ -1,5 +1,6 @@
 import React from "react";
 import {DATE_FORMAT} from "../../environment/Config";
+import {Link} from "react-router-dom";
 
 function BuddyFinderAd({userAd}) {
     return (
@@ -9,14 +10,18 @@ function BuddyFinderAd({userAd}) {
                     <div className="person-details">
                         <p>Name:</p><span>{userAd?.name}</span>
                         <p>Sex:</p><span>{userAd?.sex}</span>
-                        <p>Age:</p><span>{userAd?.age?.format(`${DATE_FORMAT}`)}</span>
+                        <p>Age:</p><span>{userAd?.age}</span>
                         <p>City:</p><span>{userAd?.city}</span>
                         <p>University:</p><span>{userAd?.university}</span>
                         <p>Languages:</p><span>{userAd?.languages}</span>
                     </div>
                     <img className="profile-picture" src={userAd?.photo} alt="User"/>
-                    <div className="date"><b>Date:</b>&ensp;</div>
-                    <button className="button-user-details element-button button-wide button-first">Details</button>
+                    <div className="date"><b>Date:</b>&ensp;{userAd?.creationDate?.format(DATE_FORMAT)}</div>
+                    <div className="button-user-details">
+                        <Link to={`/buddy/${userAd?.userAdId}`}>
+                            <button className="element-button button-wide button-first">Details</button>
+                        </Link>
+                    </div>
                 </div>
             </div>
         </>
