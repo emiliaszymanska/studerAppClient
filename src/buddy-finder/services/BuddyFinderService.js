e208dbf9e910601bf250a7b263568d255f218ef8
emiliaszymanska <esz.emilia@gmail.com>
2021-04-01 20:03:26 +0200

    BuddyFinderService.js - fetch data related to the single advertisement

--- src/buddy-finder/services/BuddyFinderService.js
+++ src/buddy-finder/services/BuddyFinderService.js
@@ -1,7 +1,12 @@
 import axios from "axios";
 import {API_URL} from "../../environment/Config";
 
-export const GetUsersAds = () => {
+export const GetAds = () => {
     return axios
         .get(`${API_URL}api/advertisement`);
+}
+
+export const GetAd = (id) => {
+    return axios
+        .get(`${API_URL}api/advertisement/${id}`);
 }
