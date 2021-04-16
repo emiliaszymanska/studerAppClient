2def088530dcb9d916d9d08d8ce8596c15bb6d84
emiliaszymanska <esz.emilia@gmail.com>
2021-04-16 11:03:50 +0200

    BuddyFinderAdDetails.js - work in progress

--- src/buddy-finder/common/BuddyFinderAdDetails.js
+++ src/buddy-finder/common/BuddyFinderAdDetails.js
@@ -1,8 +1,9 @@
 import React, {useEffect, useState} from 'react';
 import {useParams, useHistory} from "react-router-dom";
-import BuddyFinderHeader from "./common/BuddyFinderHeader";
-import {GetAd} from "./services/BuddyFinderService";
-import {AdModel} from "./models/AdModel";
+import BuddyFinderHeader from "./BuddyFinderHeader";
+import {GetAd} from "../services/BuddyFinderService";
+import {AdModel} from "../models/AdModel";
+import {DATE_FORMAT} from "../../environment/Config";
 
 function BuddyFinderAdDetails() {
 
@@ -21,10 +22,34 @@ function BuddyFinderAdDetails() {
         <>
             <section className="content-buddy-finder">
                 <div className="layout-container">
-                    <button className="element-button button-wide button-first" onClick={() => history.goBack()}>Back
-                    </button>
-                    <BuddyFinderHeader title={`Hi. I'm ${userAd?.name}`}/>
-                    {JSON.stringify(userAd)}
+                    {/*<BuddyFinderHeader title={`Hi. I'm ${userAd?.name}`}/>*/}
+                    <div className="ad-details-main-container layout-flex">
+                        <div className="ad-details-main-wrapper layout-flex">
+                            <div className="ad-details-wrapper layout-grid">
+                                <div className="ad-details-left">
+                                    <div className="ad-details-line"/>
+                                    <div className="name-user">About {userAd?.name}, {userAd?.age}</div>
+                                    {/*<p>{userAd?.title}</p>*/}
+                                    <article className="ad-description">{userAd?.description}</article>
+                                    <p>{userAd?.creationDate?.format(DATE_FORMAT)}</p>
+                                </div>
+                                <div className="ad-details-right layout-flex">
+                                    <img className="profile-picture" src={userAd?.photo} alt="User"/>
+                                    <span>{userAd?.name}</span>
+                                    <p>University:<span>{userAd?.university}Jagiellonian University</span></p>
+                                    <div className="ad-details-right-inner layout-grid">
+                                        <p>City:</p><span>{userAd?.city}</span>
+                                        <p>University:</p><span>{userAd?.university}Jagiellonian University</span>
+                                        <p>Languages:</p><span>{userAd?.languages}</span>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <button className="element-button button-wide button-first"
+                                onClick={() => history.goBack()}>Back
+                        </button>
+                    </div>
+                    {/*{JSON.stringify(userAd)}*/}
                 </div>
             </section>
         </>
