ef747af850b227a859c04bde64b4f68648e274c3
emiliaszymanska <esz.emilia@gmail.com>
2021-04-30 10:04:41 +0200

    Add BuddyFinderFilterElement.js

--- src/buddy-finder/common/BuddyFinderFilterElement.js
+++ src/buddy-finder/common/BuddyFinderFilterElement.js
@@ -2,12 +2,20 @@ import React from "react";
 import Select from "react-select";
 
 function BuddyFinderFilterElement({title, options, onChange}) {
+    const changeValue = (e) => {
+        const selectedValueLabels = e.map(item => item.label);
+        const newState = [...options];
+        newState.forEach(item => item.isSelected = selectedValueLabels.includes(item.label));
+        console.log(newState)
+        onChange(newState);
+    }
+
     return (
         <>
             <div className="select-inner-wrapper-options">
                 <div className="select-title">{title}</div>
                 <div>
-                    <Select isMulti options={options} onChange={e => onChange(e.map(item => item.value))}/>
+                    <Select isMulti options={options} onChange={e => changeValue(e)}/>
                 </div>
             </div>
         </>
