3038087d34ffe32acea180fd44fcfe11da553343
emiliaszymanska <esz.emilia@gmail.com>
2021-04-30 13:10:50 +0200

    BuddyFinder.js - apply filters

--- src/buddy-finder/BuddyFinder.js
+++ src/buddy-finder/BuddyFinder.js
@@ -8,20 +8,21 @@ import {GetAds} from "./services/BuddyFinderService";
 function BuddyFinder() {
 
     const [ads, setAds] = useState([]);
+    const [selectedFilters, setSelectedFilters] = useState([]);
 
     useEffect(() => {
-        GetAds().then(res => {
+        GetAds(selectedFilters).then(res => {
             const adsModel = res?.data?.map(item => new AdModel(item));
             setAds(adsModel);
         });
-    }, []);
+    }, [selectedFilters]);
 
     return (
         <>
             <section className="content-buddy-finder">
                 <div className="layout-container">
                     <BuddyFinderHeader title={'Meet new people'} description={'Buddy Finder – find a friend'}/>
-                    <BuddyFinderFilter/>
+                    <BuddyFinderFilter setSelectedFilters={setSelectedFilters}/>
                     {ads.map(item => <BuddyFinderAd userAd={item}/>)}
                 </div>
             </section>
