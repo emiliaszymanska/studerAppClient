93c27659434d51ccbd9c39651b65aef08fb824ee
emiliaszymanska <esz.emilia@gmail.com>
2021-05-10 23:12:16 +0200

    App.js, MainMenu.js - add link to Profile

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import ScrollRestoration from "./environment/ScrollRestoration";
 import Header from "./shared/header/Header";
 import Home from "./home/Home";
+import Profile from "./profile/Profile";
 import Map from "./map/Map";
 import Footer from "./shared/Footer";
 
@@ -13,8 +14,9 @@ function App() {
             <ScrollRestoration/>
             <Header/>
             <Switch>
-                <Route path="/" exact component={Home}/>
+                <Route path="/profile" exact component={Profile}/>
                 <Route path="/map" component={Map}/>
+                <Route path="/" exact component={Home}/>
             </Switch>
             <Footer/>
         </Router>
